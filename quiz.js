79332828f3787b23b75dd4e1e916999e21afedff
LYC <lyc242@qq.com>
2019-02-21 03:36:41 +0800

    fix bugs

--- quiz.js
+++ quiz.js
@@ -5,39 +5,54 @@ answer = prompt("PPAP I have a ____");
 
 if (answer.toUpperCase() === "PEN") {
     correctAnswer +=1;
+    answer = "";
 }
 
 answer = prompt("I have an ___");
 
-if (answer.toUpperCase()=== "APPLE"){
+if (answer.toUpperCase() === "APPLE"){
     correctAnswer +=1;
+    answer = "";
 }
 
 answer = prompt("Uh! ______");
 
-if (answer.toUpperCase() === "APPLEPEN" || "APPLE PEN" || "APPLE-PEN"){
+if (answer.toUpperCase() === "APPLEPEN"){
     correctAnswer +=1;
+    answer = "";
 }
 
 answer = prompt("I have a ____");
 
 if (answer.toUpperCase() === "PEN") {
     correctAnswer +=1;
+    answer = "";
 }
 
 answer = prompt("I have ___");
 
 if (answer.toUpperCase() === "PINEAPPLE"){
     correctAnswer +=1;
+    answer = "";
 }
 
-if (answer.toUpperCase() === "PINEAPPLEPEN" || "PINEAPPLE PEN" || "PINEAPPLE-PEN"){
+answer = prompt("Uh! _____");
+
+if (answer.toUpperCase() === "PINEAPPLEPEN"){
     correctAnswer +=1;
+    answer = "";
 }
 
-document.write("<p>You got "+correctAnswer+" question(s) right!</p>")
+document.write("<p>You got "+ correctAnswer +" question(s) right!</p>")
 
 if (correctAnswer === 6){
-    document.write("<p>You win the gold crown!</p>")
+    document.write("<p>You win the gold crown!</p>");
+}   else if (3<correctAnswer<6) {
+    document.write("<p>You win the silver crown!</p>");
+}   else if (1<correctAnswer<2) {
+    document.write("<p>You win the bronze crown!</p>");
+} else {
+    document.write("<p>Better luck next time!</p>");
 }
+
 
